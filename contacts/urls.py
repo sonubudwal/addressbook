a5b6e853c283d946467dfc1cfacba347dea71532
Fateh Budwal <sonubudwal20@gmail.com>
2015-09-26 15:00:54 -0700

    new code
    added bootstrap and edit view

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,11 +1,15 @@
 __author__ = 'fateh'
 
 from django.conf.urls import url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 import contacts.views
 
 from . import views
 
 urlpatterns = [
-   url(r'^$', contacts.views.ListContactView.as_view(),name='contacts-list',),
-   url(r'^new/$', contacts.views.CreateContactView.as_view(),name='contacts-new',),
+   url(r'^$', contacts.views.ListContactView.as_view(), name='contacts-list',),
+   url(r'^new/$', contacts.views.CreateContactView.as_view(), name='contacts-new',),
+   url(r'^edit/(?P<pk>\d+)/$', contacts.views.UpdateContactView.as_view(), name='contacts-edit',),
 ]
+urlpatterns += staticfiles_urlpatterns()
--- contacts/views.py
+++ contacts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
-from django.views.generic import CreateView
+from django.views.generic import ListView,CreateView,UpdateView
+
 from django.core.urlresolvers import reverse
 
 from contacts.models import Contact
@@ -22,3 +22,25 @@ class CreateContactView(CreateView):
 
     def get_success_url(self):
         return reverse('contacts-list')
+
+    def get_context_data(self, **kwargs):
+
+        context = super(CreateContactView, self).get_context_data(**kwargs)
+        context['action'] = reverse('contacts-new')
+        return context
+
+
+class UpdateContactView(UpdateView):
+
+    model = Contact
+    template_name = 'edit_contact.html'
+
+    def get_success_url(self):
+        return reverse('contacts-list')
+
+    def get_context_data(self, **kwargs):
+
+        context = super(UpdateContactView, self).get_context_data(**kwargs)
+        context['action'] = reverse('contacts-edit', kwargs={'pk': self.get_object().id})
+
+        return context
